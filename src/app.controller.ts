245203fbeac6e7ab4cbf2e917cb8f0231373395c
CarlosCSZ <ccherrez18@gmail.com>
2023-10-24 17:30:24 -0500

    Initial environments configurations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('octo')
+  getOcto() {
+    return this.appService.OctoService();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Octokit } from 'octokit';
+import { ConfigType } from '@nestjs/config';
+
+import config from './configurations/config';
+// import {
+//   GetResponseTypeFromEndpointMethod,
+//   OctokitResponse,
+// } from '@octokit/types';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async OctoService(): Promise<any> {
+    const octokit = new Octokit({
+      auth: this.configService.gh.token,
+    });
+    return await octokit.request('GET /repos/{owner}/{repo}', {
+      owner: this.configService.gh.owner,
+      repo: this.configService.gh.repo,
+    });
   }
 }
