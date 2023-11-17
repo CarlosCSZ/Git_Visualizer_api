3a982eb7e59ea67a97aed23082adef609cefd4b0
CarlosCSZ <ccherrez18@gmail.com>
2023-11-17 12:21:51 -0500

    FEAT: Implement a new endpoint to get commits from a priavte repo. ADD: loggers to contoller and service.

--- src/commits/commits.service.ts
+++ src/commits/commits.service.ts
@@ -1,24 +1,25 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Octokit } from 'octokit';
+import { format } from 'date-fns-tz';
 
 import { CommitDetails } from 'src/commits/models/commitDetails.model';
 import config from 'src/configurations/config';
 import { ConfigType } from '@nestjs/config';
+import { PrivateRepoDTO } from './dto/commits.dto';
 
 @Injectable()
 export class CommitsService {
   private readonly logger = new Logger(CommitsService.name);
+  private octokit: Octokit;
 
   constructor(
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
 
   async getCommitsFromRepo(repo: string): Promise<CommitDetails[]> {
-    const octokit = new Octokit({
-      auth: this.configService.gh.token,
-    });
+    this.octokit = new Octokit();
     try {
-      const ghResponse = await octokit.request(
+      const ghResponse = await this.octokit.request(
         'GET /repos/{owner}/{repo}/commits',
         {
           owner: this.configService.gh.owner,
@@ -29,11 +30,56 @@ export class CommitsService {
         return [];
       }
       const commits: CommitDetails[] = ghResponse.data.map((commit) => {
+        const dateUTC = new Date(commit.commit.committer.date);
         return {
           sha: commit.sha,
           author: commit.commit.committer.name,
           email: commit.commit.committer.email,
-          date: new Date(commit.commit.committer.date).toUTCString(),
+          date: format(dateUTC, 'EEE, dd MMM yyyy HH:mm:ss', {
+            timeZone: 'America/Guayaquil',
+          }),
+          message: commit.commit.message,
+          avatar: commit.author.avatar_url,
+          gh_url: commit.html_url,
+        };
+      });
+
+      return commits;
+    } catch (error) {
+      this.logger.error('Error getting commits: ', error.message);
+      throw new Error('404');
+    }
+  }
+
+  async commitsFromPrivateRepo(
+    privateRepo: PrivateRepoDTO,
+  ): Promise<CommitDetails[]> {
+    this.logger.log('testCommitsFromRepo INIT');
+    this.octokit = new Octokit({
+      auth: privateRepo.token,
+    });
+    try {
+      this.logger.log(this.configService.gh.token);
+      const ghResponse = await this.octokit.request(
+        'GET /repos/{owner}/{repo}/commits',
+        {
+          owner: privateRepo.owner,
+          repo: privateRepo.repo,
+        },
+      );
+
+      if (ghResponse.data.length === 0) {
+        return [];
+      }
+      const commits: CommitDetails[] = ghResponse.data.map((commit) => {
+        const dateUTC = new Date(commit.commit.committer.date);
+        return {
+          sha: commit.sha,
+          author: commit.commit.committer.name,
+          email: commit.commit.committer.email,
+          date: format(dateUTC, 'EEE, dd MMM yyyy HH:mm:ss', {
+            timeZone: 'America/Guayaquil',
+          }),
           message: commit.commit.message,
           avatar: commit.author.avatar_url,
           gh_url: commit.html_url,
--- src/commits/dto/commits.dto.ts
+++ src/commits/dto/commits.dto.ts
@@ -1,9 +1,22 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class RepoDTO {
+class RepoDTO {
   @IsNotEmpty()
   @IsString()
   @ApiProperty()
   readonly repo: string;
 }
+
+class PrivateRepoDTO extends RepoDTO {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  readonly token: string;
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  readonly owner: string;
+}
+
+export { RepoDTO, PrivateRepoDTO };
